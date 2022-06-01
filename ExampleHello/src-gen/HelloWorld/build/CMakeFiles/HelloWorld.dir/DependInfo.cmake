5afdcc24c09efc3191b3114b17520900b2309335
Benjamin Asch <benjamintasch@berkeley.edu>
2022-06-01 16:59:23 -0500

    first

---
+++ ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/HelloWorld.c" "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/HelloWorld.c.o"
+  "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/core/mixed_radix.c" "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/core/mixed_radix.c.o"
+  "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/core/platform/lf_linux_support.c" "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/core/platform/lf_linux_support.c.o"
+  "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/core/threaded/scheduler_NP.c" "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/core/threaded/scheduler_NP.c.o"
+  "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/core/utils/semaphore.c" "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/core/utils/semaphore.c.o"
+  "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/lib/schedule.c" "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/lib/schedule.c.o"
+  "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/lib/tag.c" "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/lib/tag.c.o"
+  "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/lib/time.c" "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/lib/time.c.o"
+  "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/lib/util.c" "/home/zero/lf-workspace/ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/lib/util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "LF_REACTION_GRAPH_BREADTH=1"
+  "LOG_LEVEL=2"
+  "NUMBER_OF_WORKERS=0"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../core"
+  "../core/platform"
+  "../core/federated"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ExampleHello/src-gen/HelloWorld/build/CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.dir/HelloWorld.c.o"
+  "CMakeFiles/HelloWorld.dir/core/mixed_radix.c.o"
+  "CMakeFiles/HelloWorld.dir/core/platform/lf_linux_support.c.o"
+  "CMakeFiles/HelloWorld.dir/core/threaded/scheduler_NP.c.o"
+  "CMakeFiles/HelloWorld.dir/core/utils/semaphore.c.o"
+  "CMakeFiles/HelloWorld.dir/lib/schedule.c.o"
+  "CMakeFiles/HelloWorld.dir/lib/tag.c.o"
+  "CMakeFiles/HelloWorld.dir/lib/time.c.o"
+  "CMakeFiles/HelloWorld.dir/lib/util.c.o"
+  "HelloWorld"
+  "HelloWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
